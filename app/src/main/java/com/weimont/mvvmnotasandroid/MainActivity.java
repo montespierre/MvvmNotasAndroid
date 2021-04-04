afff71a41d457ad1a3a077caa289c2b096f2e8b8
unknown <montespierre@gmail.com>
2021-04-03 19:43:34 -0500

    MVVM

---
+++ app/src/main/java/com/weimont/mvvmnotasandroid/MainActivity.java
@@ -0,0 +1,33 @@
+package com.weimont.mvvmnotasandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this, ViewModelProvider
+                .AndroidViewModelFactory
+                .getInstance(this.getApplication()))
+                .get(NoteViewModel.class);
+
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                Toast.makeText(MainActivity.this, "onChanged",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
